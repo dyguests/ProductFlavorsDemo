8a6ed15f676e615d23f98da20a489c7a6d1f730e
fanhl <fanhonglin@klicen.com>
2018-12-21 17:30:04 +0800

    info

--- app/src/main/java/com/fanhl/wechat/MainActivity.kt
+++ app/src/main/java/com/fanhl/wechat/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_info.text = ""
+        tv_info.text = WxApi.info()
     }
 }
